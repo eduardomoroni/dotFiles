556f316eaac239339007649370c1f6f938c0b140
Eduardo Moroni <eduardomoroni@gmail.com>
2018-08-18 09:47:11 -0300

    fix vim install

--- .vim/install.sh
+++ .vim/install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+VUNDLE_DIR="~/.vim/bundle/Vundle.vim"
+
+echo "$VUNDLE_DIR"
+
+if [ ! -d "$VUNDLE_DIR" ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR"
+fi
+
 vim +PluginInstall +qall
 
 echo 'Make sure you finish instalation manual step from some Vim plugin'
